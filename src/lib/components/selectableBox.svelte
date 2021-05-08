cc1133e183a6d913cf6c955fc1e056f78071eca7
EAR <e.avila@rdc-consultores.com>
2021-05-08 03:22:21 -0500

    initial commit

---
+++ src/lib/components/selectableBox.svelte
@@ -0,0 +1,59 @@
+<script>
+	/**
+	 * @type {string}
+	 */
+	export let color = 'hotpink';
+	/**
+	 * @type {string}
+	 */
+	export let hoverColor = 'yellow';
+	/**
+	 * @type {string}
+	 */
+	export let selectionColor = 'red';
+	/**
+	 * @type {number}
+	 */
+	export let scale = 1;
+	/**
+	 * @type {list}
+	 */
+	export let position = [0, 0, 0];
+	/**
+	 * If you pass an ID like '#geometry', it reuses the geometry instead of creatting a new one
+	 * @type {'box' | string}
+	 */
+	export let geometry = 'box';
+	/**
+	 * @type {bool}
+	 */
+	export let selected = false;
+
+	const onpointerenter = ({ target }) => {
+		const { object, game } = target;
+		/* three-elements bug here! */
+		object.material.color.set(hoverColor);
+		game.requestFrame();
+	};
+
+	const onpointerleave = ({ target }) => {
+		const { object, game } = target;
+		object.material.color.set(selected ? selectionColor : color);
+		game.requestFrame();
+	};
+
+	const onpointerdown = ({ target }) => {
+		const { object, game } = target;
+		object.material.color.set(selectionColor);
+		selected = !selected;
+		game.requestFrame();
+	};
+</script>
+
+<three-mesh {geometry} {position} {scale} {onpointerenter} {onpointerleave} {onpointerdown}>
+	<!-- Only adds geometry if dont pass an ID to an reusable geometry -->
+	{#if geometry === 'box' || geometry === undefined}
+		<three-box-buffer-geometry />
+	{/if}
+	<three-mesh-standard-material {color} />
+</three-mesh>
---
+++ src/routes/index.svelte
@@ -0,0 +1,54 @@
+<script>
+	import 'three-elements';
+	import SelectableElement from '$lib/components/selectableBox.svelte';
+
+	let camera = 'ortographic';
+	let nCubes = 100;
+	let domain = 100;
+</script>
+
+<div style="height: 90vh">
+	<three-game>
+		<three-scene background-color="#eee">
+			{#if camera == 'perspective'}
+				<three-perspective-camera id="camera" position="0, 0, 2000" />
+			{:else}
+				<three-orthographic-camera id="camera" position="0, 0, 2000" />
+			{/if}
+
+			<three-orbit-controls />
+
+			<three-box-buffer-geometry id="geometry" />
+			{#each new Array(nCubes) as _}
+				<SelectableElement
+					geometry="#geometry"
+					position={new Array(3).fill(0).map((v) => Math.random(1) * domain)}
+					scale="10"
+				/>
+			{/each}
+
+			<three-ambient-light intensity="0.2" />
+			<three-directional-light intensity="0.8" position="10, 10, 50" />
+		</three-scene>
+	</three-game>
+</div>
+
+<button
+	on:click={() => {
+		camera = camera == 'perspective' ? 'orthographic' : 'perspective';
+	}}
+>
+	Change Camera
+</button>
+<div>
+	Number of cubes
+	<input type="range" min="10" max="1000" bind:value={nCubes} />
+</div>
+
+<style>
+	:global(html, body) {
+		margin: 0;
+		height: 100%;
+		width: 100%;
+	}
+</style>
