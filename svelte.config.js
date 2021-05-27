f7cf73154a899a2903beab96f441e265076e6cdc
EAR <e.avila@rdc-consultores.com>
2021-05-27 13:12:41 -0500

    node adapter added

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,9 @@
+import adapter from '@sveltejs/adapter-node';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
+		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		ssr: true
